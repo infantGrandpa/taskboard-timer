823362cef7cd2b1c262483d661e24bd524e3f15e
Brett <babraham341@gmail.com>
2024-02-28 10:04:06 -0500

    Add new project to db

--- taskboard-timer/scripts/main.py
+++ taskboard-timer/scripts/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -12,6 +12,20 @@ db = SQLAlchemy(app)  # Initializes the database connection
 @app.route('/')
 def hello():
     return 'Hello, Taskboard Timer!'
+
+@app.route('/add_project', methods=['POST'])
+def add_project():
+    data = request.get_json()
+    project_name = data['name']
+
+    new_project = Project(name=project_name)
+
+    db.session.add(new_project)
+    db.session.commit()
+
+    message = f'New Project ({project_name}) added successfully!'
+
+    return jsonify({"message": message}), 201
 
 class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
