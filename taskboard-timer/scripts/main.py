266c6f18ad5961633a232abd9c165289deb9a48b
Brett <babraham341@gmail.com>
2024-02-28 09:43:12 -0500

    Create Flask app and define database

--- taskboard-timer/scripts/main.py
+++ taskboard-timer/scripts/main.py
@@ -1,7 +1,24 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+# Database Configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///taskboard.db'  # Defines the database URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disables modification notifications
+
+db = SQLAlchemy(app)  # Initializes the database connection
+
 @app.route('/')
 def hello():
-    return 'Hello, World'
+    return 'Hello, Taskboard Timer!'
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+with app.app_context():
+    db.create_all()
