47b289125a12d06868de0ad1b972d81ddb649d6b
Brett <babraham341@gmail.com>
2024-03-02 12:00:41 -0500

    Add routing after adding new project and the ability to clear new project form

--- taskboard-timer/src/components/NewProjectForm.tsx
+++ taskboard-timer/src/components/NewProjectForm.tsx
@@ -1,9 +1,11 @@
-import { Button, Stack, TextField } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Button, Container, Fab, Stack, TextField } from "@mui/material";
+import { Link, useNavigate } from "react-router-dom";
 import { ProjectCreationData, addProject } from "../services/projectService";
 import { useState } from "react";
 import { DatePicker } from "@mui/x-date-pickers";
 import { add } from "date-fns";
+import CheckIcon from "@mui/icons-material/Check";
+import ClearIcon from "@mui/icons-material/Clear";
 
 const NewProjectForm = () => {
     const [projectName, setProjectName] = useState("");
@@ -13,6 +15,8 @@ const NewProjectForm = () => {
     const [endDate, setEndDate] = useState<Date | null>(
         add(new Date(), { months: 1 })
     );
+
+    const navigate = useNavigate();
 
     const handleAddProject = async () => {
         const projectData: ProjectCreationData = {
@@ -24,28 +28,64 @@ const NewProjectForm = () => {
         };
         try {
             await addProject(projectData);
+            navigate("/");
         } catch (error) {
             console.error("Error adding new project: ", error);
         }
     };
 
+    const clearForm = async () => {
+        setProjectName("");
+        setDescription("");
+        setClient("");
+        setStartDate(new Date());
+        setEndDate(add(new Date(), { months: 1 }));
+    };
+
     return (
-        <>
-            <Button
-                component={Link}
-                to="/"
-                variant="outlined"
-                aria-label="home"
+        <Container maxWidth="sm">
+            <Stack direction="row" justifyContent="space-between">
+                <Button
+                    component={Link}
+                    to="/"
+                    variant="outlined"
+                    aria-label="home"
+                >
+                    Home
+                </Button>
+                <Button
+                    variant="outlined"
+                    onClick={clearForm}
+                    aria-label="clear form"
+                    color="error"
+                    startIcon={<ClearIcon />}
+                >
+                    Clear Form
+                </Button>
+            </Stack>
+            <Fab
+                variant="extended"
+                size="medium"
+                color="primary"
+                sx={{
+                    position: "fixed",
+                    bottom: 24,
+                    right: 24,
+                }}
+                onClick={handleAddProject}
+                aria-label="add new project"
             >
-                Home
-            </Button>
-            <Stack direction="column" spacing={2}>
+                Complete
+                <CheckIcon sx={{ ml: 1 }} />
+            </Fab>
+            <Stack direction="column" spacing={2} sx={{ pt: 2 }}>
                 <TextField
                     id="project-name-field"
                     label="Project Name"
                     variant="standard"
                     value={projectName}
                     onChange={(e) => setProjectName(e.target.value)}
+                    autoComplete="off"
                 />
                 <TextField
                     id="description-field"
@@ -56,6 +96,7 @@ const NewProjectForm = () => {
                     variant="standard"
                     value={description}
                     onChange={(e) => setDescription(e.target.value)}
+                    autoComplete="off"
                 />
                 <TextField
                     id="client-field"
@@ -63,6 +104,7 @@ const NewProjectForm = () => {
                     variant="standard"
                     value={client}
                     onChange={(e) => setClient(e.target.value)}
+                    autoComplete="off"
                 />
                 <Stack direction="row" spacing={12}>
                     <DatePicker
@@ -78,15 +120,8 @@ const NewProjectForm = () => {
                         }}
                     />
                 </Stack>
-                <Button
-                    variant="contained"
-                    onClick={handleAddProject}
-                    aria-label="add new project"
-                >
-                    Add Project
-                </Button>
             </Stack>
-        </>
+        </Container>
     );
 };
 
