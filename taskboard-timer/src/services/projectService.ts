bc69d812b1b94eb0a3a3fc5a4a095876081b6b76
Brett <babraham341@gmail.com>
2024-03-02 18:30:56 -0500

    use api client to handle the project request instead of fetch

--- taskboard-timer/src/services/projectService.ts
+++ taskboard-timer/src/services/projectService.ts
@@ -1,4 +1,5 @@
 import { Project } from "../hooks/useProjects";
+import apiClient from "./apiService";
 
 export interface ProjectCreationData {
     name: string;
@@ -8,51 +9,38 @@ export interface ProjectCreationData {
     end_date: Date | null;
 }
 
+const handleProjectRequest = async (
+    endpoint: string,
+    method: string,
+    projectData: any = null
+) => {
+    try {
+        const options = {
+            method: method,
+            headers: {
+                "Content-Type": "application/json",
+            },
+            ...(projectData &&
+                (method === "POST" || method === "PUT") && {
+                    data: projectData,
+                }),
+        };
+
+        const response = await apiClient(endpoint, options);
+
+        console.log("Server response:", response.data.message);
+        return response.data;
+    } catch (error) {
+        console.error(`Error handling project request (${method}):`, error);
+    }
+};
+
 const addProject = async (projectData: ProjectCreationData) => {
     handleProjectRequest("/api/add_project", "POST", projectData);
 };
 
 const deleteProject = async (project: Project) => {
     handleProjectRequest(`/api/project/${project.id}`, "DELETE");
-};
-
-const handleProjectRequest = async (
-    endpoint: string,
-    method: string,
-    projectData: any = null
-) => {
-    const options: {
-        method: string;
-        headers: { "Content-Type": string };
-        body?: string;
-    } = {
-        method: method,
-        headers: {
-            "Content-Type": "application/json",
-        },
-    };
-
-    // Only add the body for methods that require it
-    if (projectData && (method === "POST" || method === "PUT")) {
-        options.body = JSON.stringify(projectData);
-    }
-
-    try {
-        const response = await fetch(
-            `http://127.0.0.1:5000${endpoint}`,
-            options
-        );
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-
-        const responseData = await response.json();
-        console.log("Server response:", responseData.message);
-        return responseData; // In case you need the response data
-    } catch (error) {
-        console.error(`Error handling project request (${method}):`, error);
-    }
 };
 
 export { addProject, deleteProject };
