5cfaf2e2a19d0879cdb296bb776295cb5ff09b7a
Brett <babraham341@gmail.com>
2024-03-02 10:26:01 -0500

    Add basic fields to New Project Form

--- taskboard-timer/src/components/NewProjectForm.tsx
+++ taskboard-timer/src/components/NewProjectForm.tsx
@@ -1,7 +1,73 @@
+import { Button, Stack, TextField } from "@mui/material";
 import { Link } from "react-router-dom";
+import { ProjectCreationData, addProject } from "../services/projectService";
+import { useState } from "react";
 
 const NewProjectForm = () => {
-    return <Link to="/">Home</Link>;
+    const [projectName, setProjectName] = useState("");
+    const [description, setDescription] = useState("");
+    const [client, setClient] = useState("");
+
+    const handleAddProject = async () => {
+        const projectData: ProjectCreationData = {
+            name: projectName,
+            description: description,
+            client: client,
+            start_date: new Date(2024, 2, 1),
+            end_date: new Date(2024, 11, 31),
+        };
+        try {
+            await addProject(projectData);
+        } catch (error) {
+            console.error("Error adding new project: ", error);
+        }
+    };
+
+    return (
+        <>
+            <Button
+                component={Link}
+                to="/"
+                variant="outlined"
+                aria-label="home"
+            >
+                Home
+            </Button>
+            <Stack direction="column" spacing={2}>
+                <TextField
+                    id="project-name-field"
+                    label="Project Name"
+                    variant="standard"
+                    value={projectName}
+                    onChange={(e) => setProjectName(e.target.value)}
+                />
+                <TextField
+                    id="description-field"
+                    label="Description"
+                    multiline
+                    minRows={2}
+                    maxRows={5}
+                    variant="standard"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                />
+                <TextField
+                    id="client-field"
+                    label="Client"
+                    variant="standard"
+                    value={client}
+                    onChange={(e) => setClient(e.target.value)}
+                />
+                <Button
+                    variant="contained"
+                    onClick={handleAddProject}
+                    aria-label="add new project"
+                >
+                    Add Project
+                </Button>
+            </Stack>
+        </>
+    );
 };
 
 export default NewProjectForm;
--- taskboard-timer/src/components/ProjectsGrid.tsx
+++ taskboard-timer/src/components/ProjectsGrid.tsx
@@ -1,40 +1,14 @@
-import {
-    CircularProgress,
-    Fab,
-    Stack,
-    TextField,
-    Typography,
-} from "@mui/material";
+import { CircularProgress, Fab, Typography } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import ErrorMessage from "./ErrorMessage";
 import useProjects from "../hooks/useProjects";
 import ProjectCard from "./ProjectCard";
 import Grid from "@mui/material/Unstable_Grid2";
 import DynamicTable from "./DynamicTable";
-import { useState } from "react";
-import { addProject, ProjectCreationData } from "../services/projectService";
 import { Link } from "react-router-dom";
 
 const ProjectsGrid = () => {
     const { data, isLoading, error, refetch } = useProjects();
-    const [projectName, setProjectName] = useState("");
-
-    const handleAddProject = async () => {
-        const projectData: ProjectCreationData = {
-            name: projectName,
-            description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sed quam nec erat varius molestie.",
-            client: "Just me!",
-            start_date: new Date(2024, 2, 1),
-            end_date: new Date(2024, 11, 31),
-        };
-        try {
-            await addProject(projectData);
-            refetch();
-        } catch (error) {
-            console.error("Error adding new project: ", error);
-        }
-    };
 
     const projectsColumns = [
         { field: "id", label: "ID" },
@@ -50,21 +24,12 @@ const ProjectsGrid = () => {
             <Typography variant="h2" sx={{ pt: 2 }}>
                 Total Projects: {data?.length}
             </Typography>
-            <Link to="/new-project">Create New Project</Link>
-            <Stack direction="row">
-                <TextField
-                    id="project-name"
-                    label="Project Name"
-                    variant="standard"
-                    value={projectName}
-                    onChange={(e) => setProjectName(e.target.value)}
-                />
-            </Stack>
             <Fab
+                component={Link}
+                to="/new-project"
                 color="primary"
-                aria-label="add new project"
+                aria-label="create new project"
                 size="large"
-                onClick={handleAddProject}
                 sx={{
                     position: "fixed",
                     bottom: 24,
