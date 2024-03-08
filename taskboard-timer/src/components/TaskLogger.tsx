b092aba7c7e4dc69d0e02841d49f00bcba8f0a05
Brett <babraham341@gmail.com>
2024-03-08 12:58:08 -0500

    Pass TaskQuery from TaskLogger to TaskContext

--- taskboard-timer/src/components/TaskLogger.tsx
+++ taskboard-timer/src/components/TaskLogger.tsx
@@ -1,32 +1,90 @@
 import { useEffect } from "react";
 import { useTaskContext } from "./TaskProvider";
-import { Button, Stack, Typography } from "@mui/material";
+import {
+    Button,
+    List,
+    ListItem,
+    ListItemText,
+    Stack,
+    Typography,
+} from "@mui/material";
+import { TaskQuery } from "../hooks/useTasks";
 
 const TaskLogger = () => {
-    const { refreshNeeded, triggerRefresh, resetRefresh } = useTaskContext();
+    const {
+        data,
+        isLoading,
+        error,
+        refreshNeeded,
+        triggerRefresh,
+        resetRefresh,
+        taskQuery,
+        setTaskQuery,
+    } = useTaskContext();
 
     useEffect(() => {
+        console.log("USE EFFECT TRIGGERED");
         console.log(`refreshNeeded: ${refreshNeeded}`);
-    }, [refreshNeeded]);
+        console.log("DATA");
+        console.log(data);
+        console.log(`isLoading: ${isLoading}`);
+        console.log(`error: ${error}`);
+    }, [taskQuery]);
+
+    const handleQueryChange = () => {
+        if (taskQuery?.project_id === 17) {
+            setTaskQuery({ project_id: null } as TaskQuery);
+        } else {
+            setTaskQuery({ project_id: 17 } as TaskQuery);
+        }
+    };
 
     return (
-        <Stack
-            direction="column"
-            alignItems="center"
-            spacing={2}
-            sx={{ mt: 3, mb: 5 }}
-        >
-            <Typography variant="h5">
-                {refreshNeeded ? "Refresh is needed!" : "No refresh needed."}
-            </Typography>
+        <Stack direction="column" spacing={2} sx={{ mt: 3, mb: 5 }}>
+            <Stack
+                direction="row"
+                justifyContent="space-between"
+                alignItems="flex-end"
+            >
+                <Typography variant="h5">
+                    {refreshNeeded ? "Refresh Needed" : "No Refresh Needed"}
+                </Typography>
+                <Typography variant="body1">
+                    Project ID: {taskQuery?.project_id}
+                </Typography>
+            </Stack>
+
             <Stack direction="row" spacing={2}>
                 <Button onClick={triggerRefresh} variant="contained">
                     Trigger Refresh
+                </Button>
+                <Button
+                    onClick={handleQueryChange}
+                    variant="contained"
+                    color="secondary"
+                >
+                    Toggle Query
                 </Button>
                 <Button onClick={resetRefresh} variant="outlined">
                     Reset Refresh
                 </Button>
             </Stack>
+            <Typography variant="h6">Task List ({data?.length})</Typography>
+            <List
+                dense
+                sx={{ backgroundColor: "secondary.dark", borderRadius: 1 }}
+            >
+                {data &&
+                    data.map((task) => (
+                        <ListItem key={task.id}>
+                            <ListItemText>
+                                {task.name.length > 0
+                                    ? `${task.name} (${task.id})`
+                                    : `Task ${task.id}`}
+                            </ListItemText>
+                        </ListItem>
+                    ))}
+            </List>
         </Stack>
     );
 };
--- taskboard-timer/src/components/TaskProvider.tsx
+++ taskboard-timer/src/components/TaskProvider.tsx
@@ -1,15 +1,26 @@
 import { ReactNode, createContext, useContext, useState } from "react";
+import useTasks, { Task, TaskQuery } from "../hooks/useTasks";
 
 interface TaskContentType {
+    taskQuery?: TaskQuery;
+    setTaskQuery: (query: TaskQuery) => void;
+    data: Task[] | null;
+    isLoading: boolean;
+    error: string | undefined;
     triggerRefresh: () => void;
     resetRefresh: () => void;
     refreshNeeded: boolean;
 }
 
 const TaskContext = createContext<TaskContentType>({
+    taskQuery: undefined,
+    setTaskQuery: () => void 0,
     triggerRefresh: () => void 0,
     resetRefresh: () => void 0,
     refreshNeeded: false,
+    data: null,
+    isLoading: false,
+    error: undefined,
 });
 
 interface Props {
@@ -18,13 +29,27 @@ interface Props {
 
 const TaskProvider = ({ children }: Props) => {
     const [refreshNeeded, setRefreshNeeded] = useState(false);
+    const [taskQuery, setTaskQuery] = useState<TaskQuery>({
+        project_id: 17,
+    } as TaskQuery);
+
+    const { data, isLoading, error } = useTasks(taskQuery);
 
     const triggerRefresh = () => setRefreshNeeded(true);
     const resetRefresh = () => setRefreshNeeded(false);
 
     return (
         <TaskContext.Provider
-            value={{ triggerRefresh, resetRefresh, refreshNeeded }}
+            value={{
+                taskQuery: taskQuery,
+                setTaskQuery: setTaskQuery,
+                data: data,
+                isLoading: isLoading,
+                error: error,
+                triggerRefresh: triggerRefresh,
+                resetRefresh: resetRefresh,
+                refreshNeeded: refreshNeeded,
+            }}
         >
             {children}
         </TaskContext.Provider>
