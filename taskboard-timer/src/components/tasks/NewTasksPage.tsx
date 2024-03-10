ddd2431394274479cc9b7838db438d6600db787f
Brett <babraham341@gmail.com>
2024-03-10 12:43:07 -0400

    Add task table to NewTasksPage

--- taskboard-timer/src/components/tasks/NewTasksPage.tsx
+++ taskboard-timer/src/components/tasks/NewTasksPage.tsx
@@ -1,10 +1,20 @@
 import { Typography } from "@mui/material";
 import { useParams } from "react-router-dom";
+import { TaskProvider } from "../../providers/TaskProvider";
+import NewTaskTable from "./NewTaskTable";
+import { TaskQuery } from "../../hooks/useTasks";
 
 const NewTasksPage = () => {
     let { id } = useParams();
     return (
-        <Typography variant="h2">Create New Tasks for Project {id}</Typography>
+        <>
+            <Typography variant="h2">
+                Create New Tasks for Project {id}
+            </Typography>
+            <TaskProvider initialTaskQuery={{ project_id: id } as TaskQuery}>
+                <NewTaskTable />
+            </TaskProvider>
+        </>
     );
 };
 
