2c9f897c9c679a9d40fc4b1f21863abd1326011c
Brett <babraham341@gmail.com>
2024-04-12 12:41:22 -0400

    Add context menu when clicking a project card

--- taskboard-timer/src/components/projects/ProjectCard.tsx
+++ taskboard-timer/src/components/projects/ProjectCard.tsx
@@ -2,6 +2,8 @@ import {
     Card,
     CardActionArea,
     CardContent,
+    Menu,
+    MenuItem,
     Stack,
     SxProps,
     Typography,
@@ -11,6 +13,7 @@ import { Link } from "react-router-dom";
 import DateRange from "../DateRange";
 import { Project } from "../../constants/projects";
 import routes from "../../constants/routes";
+import React, { useState } from "react";
 
 interface Props {
     project: Project;
@@ -20,51 +23,88 @@ interface Props {
 
 const ProjectCard = ({ project, variant, sx }: Props) => {
     const isFeatured = variant === "featured";
+    const [contextMenu, setContextMenu] = useState<{
+        mouseX: number;
+        mouseY: number;
+    } | null>(null);
+
+    const handleContextMenu = (event: React.MouseEvent) => {
+        event.preventDefault();
+        setContextMenu(
+            contextMenu === null
+                ? {
+                      mouseX: event.clientX + 2,
+                      mouseY: event.clientY - 6,
+                  }
+                : null
+        );
+    };
+
+    const handleCloseContextMenu = () => {
+        setContextMenu(null);
+    };
 
     return (
-        <Card
-            sx={{
-                ...sx,
-                display: "flex",
-                width: "100%",
-            }}
-        >
-            <CardActionArea
-                component={Link}
-                to={routes.project(project.id)}
-                sx={{ ...(isFeatured && { height: "100%" }) }}
+        <div onContextMenu={handleContextMenu}>
+            <Menu
+                open={contextMenu !== null}
+                onClose={handleCloseContextMenu}
+                anchorReference="anchorPosition"
+                anchorPosition={
+                    contextMenu !== null
+                        ? { top: contextMenu.mouseY, left: contextMenu.mouseX }
+                        : undefined
+                }
             >
-                <CardContent>
-                    <Stack
-                        direction="row"
-                        justifyContent="space-between"
-                        alignItems="flex-start"
-                    >
-                        <Typography
-                            variant={isFeatured ? "h4" : "h5"}
-                            component="div"
+                <MenuItem onClick={handleCloseContextMenu}>Test Item</MenuItem>
+            </Menu>
+            <Card
+                sx={{
+                    ...sx,
+                    display: "flex",
+                    width: "100%",
+                }}
+            >
+                <CardActionArea
+                    component={Link}
+                    to={routes.project(project.id)}
+                    sx={{ ...(isFeatured && { height: "100%" }) }}
+                >
+                    <CardContent>
+                        <Stack
+                            direction="row"
+                            justifyContent="space-between"
+                            alignItems="flex-start"
                         >
-                            {project.name}
+                            <Typography
+                                variant={isFeatured ? "h4" : "h5"}
+                                component="div"
+                            >
+                                {project.name}
+                            </Typography>
+                            <Typography
+                                variant="caption"
+                                color="text.secondary"
+                            >
+                                {project.id}
+                            </Typography>
+                        </Stack>
+                        <Typography variant="subtitle1" color="text.secondary">
+                            {project.client}
                         </Typography>
-                        <Typography variant="caption" color="text.secondary">
-                            {project.id}
+                        <Typography variant="body2">
+                            {project.description}
                         </Typography>
-                    </Stack>
-                    <Typography variant="subtitle1" color="text.secondary">
-                        {project.client}
-                    </Typography>
-                    <Typography variant="body2">
-                        {project.description}
-                    </Typography>
-                    <Typography variant="subtitle2" sx={{ pt: 2 }}>
-                        <DateRange
-                            startDate={project.start_date}
-                            endDate={project.end_date}
-                        />
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
-        </Card>
+                        <Typography variant="subtitle2" sx={{ pt: 2 }}>
+                            <DateRange
+                                startDate={project.start_date}
+                                endDate={project.end_date}
+                            />
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+        </div>
     );
 };
 
