acb8038c2b9c5d8f7347bcfc8a025b62a8298e2a
Brett <babraham341@gmail.com>
2024-04-20 10:46:27 -0400

    Replace "no tasks" error message with a centered icon and large text

--- taskboard-timer/src/components/tasks/NewTaskTable.tsx
+++ taskboard-timer/src/components/tasks/NewTaskTable.tsx
@@ -1,4 +1,3 @@
-import ErrorMessage from "../ErrorMessage";
 import LoadingBackdrop from "../LoadingBackdrop";
 import Grid from "@mui/material/Unstable_Grid2";
 import NewTask from "./NewTask";
@@ -6,6 +5,7 @@ import TaskRow from "./TaskRow";
 import { useTaskContext } from "../../providers/TaskProvider";
 import { Divider, Stack, Typography } from "@mui/material";
 import { StatusAlert } from "../StatusAlert";
+import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
 
 const NewTaskTable = () => {
     const { data, isLoading, message, status, taskQuery } = useTaskContext();
@@ -27,19 +27,33 @@ const NewTaskTable = () => {
                 {projectId && <NewTask projectId={projectId} />}
             </Stack>
             <Divider sx={{ my: 1 }} />
-            {data && (
-                <Grid container spacing={1}>
-                    {data.length > 0 ? (
-                        data.map((task) => (
-                            <Grid key={task.id} xs={12}>
-                                <TaskRow task={task} />
-                            </Grid>
-                        ))
-                    ) : (
-                        <ErrorMessage message="No tasks yet!" />
-                    )}
-                </Grid>
-            )}
+
+            <Grid container spacing={1}>
+                {data && data.length > 0 ? (
+                    data.map((task) => (
+                        <Grid key={task.id} xs={12}>
+                            <TaskRow task={task} />
+                        </Grid>
+                    ))
+                ) : (
+                    <Stack
+                        direction="column"
+                        justifyContent="center"
+                        alignItems="center"
+                        sx={{
+                            width: "100%",
+                            minHeight: "400px",
+                        }}
+                    >
+                        <ErrorOutlineIcon
+                            sx={{ fontSize: "3rem", mb: 1, color: "#A1A1A1" }}
+                        />
+                        <Typography variant="h5" sx={{ color: "#A1A1A1" }}>
+                            No tasks yet!
+                        </Typography>
+                    </Stack>
+                )}
+            </Grid>
         </>
     );
 };
