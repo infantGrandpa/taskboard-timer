454b244b5482d713d2401388681507f66eade661
Brett <babraham341@gmail.com>
2024-04-20 11:06:51 -0400

    Refactor Task Table for a better layout

--- taskboard-timer/src/components/tasks/NewTaskTable.tsx
+++ taskboard-timer/src/components/tasks/NewTaskTable.tsx
@@ -3,7 +3,7 @@ import Grid from "@mui/material/Unstable_Grid2";
 import NewTask from "./NewTask";
 import TaskRow from "./TaskRow";
 import { useTaskContext } from "../../providers/TaskProvider";
-import { Divider, Stack, Typography } from "@mui/material";
+import { Container, Divider, Stack, Typography } from "@mui/material";
 import { StatusAlert } from "../StatusAlert";
 import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
 
@@ -13,48 +13,55 @@ const NewTaskTable = () => {
     const projectId = taskQuery?.project_id;
 
     return (
-        <>
-            {isLoading && <LoadingBackdrop />}
-            {status && <StatusAlert status={status} message={message} />}
+        <Stack
+            direction="column"
+            alignItems="stretch"
+            divider={<Divider flexItem />}
+            justifyContent="flex-start"
+            spacing={1}
+            sx={{
+                minHeight: "100%",
+            }}
+        >
             <Stack
                 direction="row"
                 justifyContent="space-between"
                 alignItems="center"
             >
-                <Typography variant="h5">
-                    Tasks for Project {projectId}
-                </Typography>
+                <Typography variant="h5">All Tasks</Typography>
                 {projectId && <NewTask projectId={projectId} />}
             </Stack>
-            <Divider sx={{ my: 1 }} />
 
-            <Grid container spacing={1}>
-                {data && data.length > 0 ? (
-                    data.map((task) => (
+            {data && data.length > 0 ? (
+                <Grid container spacing={1}>
+                    {data.map((task) => (
                         <Grid key={task.id} xs={12}>
                             <TaskRow task={task} />
                         </Grid>
-                    ))
-                ) : (
-                    <Stack
-                        direction="column"
-                        justifyContent="center"
-                        alignItems="center"
-                        sx={{
-                            width: "100%",
-                            minHeight: "400px",
-                        }}
-                    >
-                        <ErrorOutlineIcon
-                            sx={{ fontSize: "3rem", mb: 1, color: "#A1A1A1" }}
-                        />
-                        <Typography variant="h5" sx={{ color: "#A1A1A1" }}>
-                            No tasks yet!
-                        </Typography>
-                    </Stack>
-                )}
-            </Grid>
-        </>
+                    ))}
+                </Grid>
+            ) : (
+                <Stack
+                    direction="column"
+                    justifyContent="center"
+                    alignItems="center"
+                    sx={{
+                        flexGrow: "1",
+                    }}
+                >
+                    {isLoading && <LoadingBackdrop />}
+                    {status && (
+                        <StatusAlert status={status} message={message} />
+                    )}
+                    <ErrorOutlineIcon
+                        sx={{ fontSize: "3rem", mb: 1, color: "#A1A1A1" }}
+                    />
+                    <Typography variant="h5" sx={{ color: "#A1A1A1" }}>
+                        No tasks yet!
+                    </Typography>
+                </Stack>
+            )}
+        </Stack>
     );
 };
 
